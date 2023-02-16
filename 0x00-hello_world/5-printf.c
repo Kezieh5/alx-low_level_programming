937e7c440880d489cd3a95ac9a5e019bd6ac51d1
Kezieh5 <mwangikezzieh@gmail.com>
2023-02-16 11:40:06 -0800

    fdf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - prints a statement followed by a new line
- * Return: Always 0
+ * main - Prints out a string to stdout
+ * Return: 0 if success.
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,n\");
+printf("with proper grammar,but the outcome is a piece of art,\n");
 return (0);
-}
