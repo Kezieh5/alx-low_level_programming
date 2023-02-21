63ed87af3899c0708762db3f4525014da9c73a0a
Kezieh5 <mwangikezzieh@gmail.com>
2023-02-21 02:20:23 -0800

    103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints the sum of fibonacci numbers
+ * main - prints the sum of Even fibonacci numbers
  * less than 4000000.
  * Return: Nothing!
  */
@@ -9,7 +9,7 @@ int main(void)
 	int i = 0;
 	long j = 1, k = 2, sum = k;
 
-	while (k = j < 4000000)
+	while (k + j < 4000000)
 	{
 	k += j;
 	if (k % 2 == 0)
