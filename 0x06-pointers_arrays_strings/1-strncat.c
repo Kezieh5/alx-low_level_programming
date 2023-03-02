11f5ec7a828d166a428a041fd909bbf30ee49eb4
Kezieh5 <mwangikezzieh@gmail.com>
2023-03-01 21:03:26 -0800

    1-srrncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,16 +1,11 @@
 #include "main.h"
 /**
  * _strncat - concanates two strings at most
- * 
  * an inputed number of bytes from src
- * 
  * @dest: the string to be appended upon
- * 
  * @src: the string to be appended to dest
- * 
  * @n: the number of bytes fro src to be appended on dest
- *
- * * Return: a pointer to the string dest
+ * Return: a pointer to the string dest
  */
 
 char *_strncat(char *dest, char *src, int n)
