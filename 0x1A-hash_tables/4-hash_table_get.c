e4ce2291697b74b863882f29d3c885738a1764b7
Kezieh5 <mwangikezzieh@gmail.com>
2023-06-08 23:56:53 -0700

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,29 +1,34 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - retrieves a value associated with a key
- * @ht: the hash table
- * @key: the key
- * Return: the value or null
+ * hash_table_get - function to get a value of a key in a hash table
+ * @ht: type pointer const char of the hash_table
+ * @key: type pointer char the key of the hash table
+ * Return: Null if the key is not found or the value of the key
  */
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int ind;
-	hash_node_t *current;
+	unsigned long int idx;
+	hash_node_t *current_node;
 
-	ind = key_index((const unsigned char *)key, ht->size);
-	if (ind >= ht->size)
+	if (ht == NULL || *key == 48 || key == NULL)
+		return (NULL);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	if (idx >= ht->size)
+		return (NULL);
+
+	for (current_node = ht->array[idx];
+	     current_node && strcmp(current_node->key, key) != 0;
+	     current_node = current_node->next)
+		;
+	if (current_node == NULL)
 	{
 		return (NULL);
 	}
-	current = ht->array[ind];
-	while (current && strcmp(current->key, key) != 0)
+	else
 	{
-		current = current->next;
+		return (current_node->value);
 	}
-	if (current == NULL)
-	{
-		return (NULL);
-	}
-	return (current->value);
 }
